96c4fc3f81efc499dd2bb2c6f1e3e73259e009e1
hnsia <44498178+hnsia@users.noreply.github.com>
2024-06-02 19:57:35 +0800

    feat: add prompt to create bill

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+
+	b := newBill(name)
+	fmt.Println("Created the bill - ", b.name)
+
+	return b
+}
 
 func main () {
+	// receiverFuncRoutine()
+	mybill := createBill()
+
+	fmt.Println(mybill)
+}
+
+func receiverFuncRoutine() {
 	mybill := newBill("mario's bill")
 
 	mybill.addItem("onion soup", 4.50)
