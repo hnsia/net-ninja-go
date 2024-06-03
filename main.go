d814e14700cf323d899952168a85d7f4e5bafcbb
hnsia <44498178+hnsia@users.noreply.github.com>
2024-06-03 11:55:26 +0800

    feat: add option prompt

--- main.go
+++ main.go
@@ -17,10 +17,6 @@ func getInput(prompt string, r *bufio.Reader) (string, error) {
 func createBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 
-	// fmt.Print("Create a new bill name: ")
-	// name, _ := reader.ReadString('\n')
-	// name = strings.TrimSpace(name)
-
 	name, _ := getInput("Create a new bill name: ", reader)
 
 	b := newBill(name)
@@ -29,11 +25,17 @@ func createBill() bill {
 	return b
 }
 
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	opt, _ := getInput("Choose option (a - add item, s - save bill, t - add tip): ", reader)
+	fmt.Println(opt)
+}
+
 func main () {
 	// receiverFuncRoutine()
 	mybill := createBill()
-
-	fmt.Println(mybill)
+	promptOptions(mybill)
 }
 
 func receiverFuncRoutine() {
